b6e4cc8b380fed9cbf9bf57cfe77fe3f9eb5c3c9
Urban Bruhin <urban.bruhin@sensirion.com>
2020-03-24 14:32:55 +0100

    Clean up test configuration
    For consistency with other, similar projects.

--- conftest.py
+++ conftest.py
@@ -5,35 +5,44 @@ from __future__ import absolute_import, division, print_function
 from sensirion_shdlc_driver import ShdlcSerialPort, ShdlcConnection
 from sensirion_shdlc_sensorbridge import SensorBridgePort, \
     SensorBridgeShdlcDevice
-import platform
 import pytest
-
-
-# Choose a reasonable default serial port where the SensorBridge could be
-# connected, depending on the operating system.
-if platform.system().lower() == 'windows':
-    DEFAULT_PORT = 'COM2'  # COM1 is usually the built-in port, so skip it
-else:
-    DEFAULT_PORT = '/dev/ttyUSB0'  # First virtual serial port
 
 
 def pytest_addoption(parser):
     """
     Register command line options
     """
-    parser.addoption("--serial-port", action="store", default=DEFAULT_PORT)
+    parser.addoption("--serial-port", action="store", type=str)
+
+
+def _get_serial_port(config, validate=False):
+    """
+    Get the serial port to be used for the tests.
+    """
+    port = config.getoption("--serial-port")
+    if (validate is True) and (port is None):
+        raise ValueError("Please specify the serial port to be used with "
+                         "the '--serial-port' argument.")
+    return port
 
 
 def pytest_report_header(config):
     """
     Add extra information to test report header
     """
-    return "serial port: " + config.getoption("--serial-port")
+    return "serial port: " + str(_get_serial_port(config))
 
 
 @pytest.fixture(scope="session")
-def connection(request):
-    serial_port = request.config.getoption("--serial-port")
+def serial_port(request):
+    """
+    Fixture to get the serial port to be used for the tests.
+    """
+    return _get_serial_port(request.config, validate=True)
+
+
+@pytest.fixture(scope="session")
+def connection(serial_port):
     with ShdlcSerialPort(serial_port, 460800) as port:
         connection = ShdlcConnection(port)
         yield connection
