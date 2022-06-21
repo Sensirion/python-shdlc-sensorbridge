ea5080523cad169eb7ea356bd92514cf789afa27
Urban Bruhin <urban.bruhin@sensirion.com>
2022-06-21 16:01:39 +0200

    Update some metadata according package template

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ long_description = readme.strip() + "\n\n" + changelog.strip() + "\n"
 setup(
     name='sensirion-shdlc-sensorbridge',
     version=version_string,
-    author='Urban Bruhin',
-    author_email='urban.bruhin@sensirion.com',
+    author='Sensirion',
+    author_email='info@sensirion.com',
     description='Driver for Communicating With Sensirion SEK-SensorBridge',
     license='BSD',
     keywords='shdlc sensirion sensor bridge sensorbridge driver',
@@ -54,6 +54,9 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: System :: Hardware :: Hardware Drivers'
     ]
 )
